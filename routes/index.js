da15a4e76111b7398e329eaab5bba9210be0c1a0
Wenuka19 <89344987+Wenuka19@users.noreply.github.com>
2022-07-27 11:45:15 +0530

    Init Check

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const router = express.Router()
 
 router.get('/', (req, res) => {
-    res.send("Welcome to the server")
+    res.send("Welcome to the Hospital Management Server")
 })
 
 module.exports = router
